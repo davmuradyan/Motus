153bde397502fe9cf488f7d4f08569242e84a5a1
Your Name <your_email@example.com>
2024-12-15 23:51:35 +0400

    dd

--- Assets/FlappyBird/Scripts/Global/ISpawnable.cs
+++ Assets/FlappyBird/Scripts/Global/ISpawnable.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 internal interface ISpawnable {
     public bool isAvailable { get; set; }
     public bool IsAvailable();
-
     public float speed { get; set; }
     public GameObject gameObject {  get; set; }
 }
